b022c929beaef8a7219b5cea969d1331458b0d02
kjura <30084359+kjura@users.noreply.github.com>
2024-12-18 22:08:09 +0100

    feat: add numeral to roman conversion

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::cmp;
 use std::collections::HashMap;
 
 fn roman_as_num(roman: &str) -> u64 {
-
+    // Thanks a lot! -> http://blog.functionalfun.net/2009/01/project-euler-89-converting-to-and-from.html
     let roman_to_numerals = HashMap::from(
         [
             ('I', 1u64),
@@ -37,14 +37,106 @@ fn roman_as_num(roman: &str) -> u64 {
 
 }
 
+fn num_as_roman(decimal_number: u16) -> String{
+    // Thanks! -> https://stackoverflow.com/questions/9083037/convert-a-number-into-a-roman-numeral-in-javascript
+    // {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
+    let romans_to_numerals = HashMap::from(
+        [
+            ("M", 1000u16),
+            ("CM", 900),
+            ("D", 500),
+            ("CD", 400),
+            ("C", 100),
+            ("XC", 90),
+            ("L", 50),
+            ("XL", 40),
+            ("X", 10),
+            ("IX", 9),
+            ("V", 5),
+            ("IV", 4),
+            ("I", 1)
+        ]
+    );
+
+    let mut _decimal_iterator = decimal_number;
+    let mut roman_number= String::from("");
+    for i in vec!["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"] {
+        while _decimal_iterator >= romans_to_numerals[i] {
+            roman_number += i;
+            _decimal_iterator -= romans_to_numerals[i];
+        }
+    }
+
+    roman_number
+     
+
+
+}
 
 fn main() {
 
-    let result = roman_as_num("XLIV");
-    println!("{}", result)
+    let number_devil = 666;
+    let roman_heaven = "XLIV";
 
-    // [5, 1, 50, 10] --> 0 + 5 - 1 + 50 - 10 = 44
-    // acc = 0
-    // if max( if max(0, 5) >= 5 { acc + 5 } else { acc - 5 } ) >= 1 { acc + 1 } else { acc - 1 }
+    println!("Number from heaven XLIV is {}", roman_as_num(roman_heaven));
+    println!("Number from hell 666 is roman {}", num_as_roman(number_devil))
+}
 
+#[cfg(test)]
+mod tests_num_as_roman {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        let result = num_as_roman(44);
+        assert_eq!(result, "XLIV");
+    }
+
+    #[test]
+    fn case_2() {
+        let result = num_as_roman(666);
+        assert_eq!(result, "DCLXVI");
+    }
+
+    #[test]
+    fn case_3() {
+        let result = num_as_roman(99);
+        assert_eq!(result, "XCIX");
+    }
+
+    #[test]
+    fn case_4() {
+        let result = num_as_roman(55);
+        assert_eq!(result, "LV");
+    }
+}
+
+#[cfg(test)]
+mod tests_roman_as_num {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        let result = roman_as_num("V");
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn case_2() {
+        let result = roman_as_num("DCLXVI");
+        assert_eq!(result, 666);
+    }
+
+    #[test]
+    fn case_3() {
+        let result = roman_as_num("XCIX");
+        assert_eq!(result, 99);
+    }
+
+    #[test]
+    fn case_4() {
+        let result = roman_as_num("LV");
+        assert_eq!(result, 55);
+
+}
 }
